7d792e616e721e41ff1c40f9a7a6f9285dc9cff2
hernan.wilkinson <hernan.wilkinson@10pines.com>
2021-10-29 16:22:14 -0300

    Test: Carrito deja de estar vacio cuando se agrega un producto

--- specs/unit/Cart.spec.js
+++ specs/unit/Cart.spec.js
@@ -1,9 +1,22 @@
+import {createiPad} from "./TestObejectsFactory";
+
 class Cart {
     isEmpty() {
         return true;
+    }
+
+    addProduct(product) {
+
     }
 }
 
 test('Carrito se crea vacio', () => {
     expect(new Cart().isEmpty()).toBeTruthy()
+})
+
+test('Carrito deja de estar vacio cuando se agrega un producto', () => {
+    const iPad = createiPad()
+    const cart = new Cart();
+    cart.addProduct(iPad)
+    expect(cart.isEmpty()).toBeFalsy()
 })
