498c6394e50b29b9019dc555f4a7783a1879c623
hernan.wilkinson <hernan.wilkinson@10pines.com>
2021-10-29 17:10:32 -0300

    Pass: availableProducts devuelve los productos con stock

--- specs/unit/Supermarket.spec.js
+++ specs/unit/Supermarket.spec.js
@@ -1,19 +1,21 @@
 import {createiPad, createTShirt} from "./TestObejectsFactory";
 
 class Supermarket {
+
     constructor(products) {
+        this._products = products
 
     }
 
     availableProducts() {
-        return undefined;
+        return this._products
     }
 }
 
 test('availableProducts devuelve los productos con stock', () => {
-    const tShirt = createTShirt();
-    const iPad = createiPad();
+    const tShirt = createTShirt()
+    const iPad = createiPad()
     const supermarket = new Supermarket([tShirt,iPad])
 
-    expect(supermarket.availableProducts()).toBe([tShirt,iPad])
+    expect(supermarket.availableProducts()).toStrictEqual([tShirt,iPad])
 })
