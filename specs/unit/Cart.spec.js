e9cbc59c0cb100749a5f590f0a59f92b14017467
hernan.wilkinson <hernan.wilkinson@10pines.com>
2021-10-29 16:52:28 -0300

    Refactor: Mover Cart a Cart.js

--- specs/unit/Cart.spec.js
+++ specs/unit/Cart.spec.js
@@ -1,46 +1,5 @@
 import {createiPad, createTShirt} from "./TestObejectsFactory";
-import {CartItem} from "../../src/model/CartItem";
-
-class Cart {
-    constructor() {
-        this._contents = []
-    }
-
-    isEmpty() {
-        return this._contents.length === 0;
-    }
-
-    addProduct(product) {
-        this.withCartItemOfProductDo(
-            product,
-            cartItem => cartItem.incrementQuantity(),
-            () => this._contents.push(new CartItem(product)));
-    }
-
-    quantityOf(product) {
-        return this.withCartItemOfProductDo(
-            product,
-            cartItem => cartItem.quantity(),
-            () => 0
-        )
-    }
-
-    contents() {
-        return this._contents
-    }
-
-    withCartItemOfProductDo(product, ifFound, ifNotFound) {
-        const cartItem = this._contents.find(cartItem => cartItem.isFor(product))
-        if (!cartItem)
-            return ifNotFound()
-        else
-            return ifFound(cartItem)
-    }
-
-    total() {
-        return this._contents.reduce((total, cartItem) => total + cartItem.total(), 0)
-    }
-}
+import {Cart} from "../../src/model/Cart";
 
 test('Carrito se crea vacio', () => {
     expect(new Cart().isEmpty()).toBeTruthy()
---
+++ src/model/Cart.js
@@ -0,0 +1,42 @@
+import {CartItem} from "./CartItem";
+
+export class Cart {
+    constructor() {
+        this._contents = []
+    }
+
+    isEmpty() {
+        return this._contents.length === 0;
+    }
+
+    addProduct(product) {
+        this.withCartItemOfProductDo(
+            product,
+            cartItem => cartItem.incrementQuantity(),
+            () => this._contents.push(new CartItem(product)));
+    }
+
+    quantityOf(product) {
+        return this.withCartItemOfProductDo(
+            product,
+            cartItem => cartItem.quantity(),
+            () => 0
+        )
+    }
+
+    contents() {
+        return this._contents
+    }
+
+    withCartItemOfProductDo(product, ifFound, ifNotFound) {
+        const cartItem = this._contents.find(cartItem => cartItem.isFor(product))
+        if (!cartItem)
+            return ifNotFound()
+        else
+            return ifFound(cartItem)
+    }
+
+    total() {
+        return this._contents.reduce((total, cartItem) => total + cartItem.total(), 0)
+    }
+}
