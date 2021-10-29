78dde4951c433c7eb62ca01b64a310090a2ca47b
hernan.wilkinson <hernan.wilkinson@10pines.com>
2021-10-29 09:20:08 -0300

    Refactor: Extract creación de productos

--- specs/unit/Test1.spec.js
+++ specs/unit/Test1.spec.js
@@ -1,27 +1,35 @@
 import {CartItem} from "../../src/model/CartItem";
 
+function iPad() {
+    return {id: 1, title: "iPad 4 Mini", price: 500.01, inventory: 2}
+}
+
+let tShirt = function () {
+    return {id: 2, title: "H&M T-Shirt White", price: 10.99, inventory: 10}
+}
+
 test('Los cart items se crean con un producto', () => {
-    const product = 'a product';
+    const product = iPad();
     const cartItem = new CartItem(product);
     expect(cartItem.isFor(product)).toBeTruthy()
-    expect(cartItem.isFor('another product')).toBeFalsy()
+    expect(cartItem.isFor(tShirt())).toBeFalsy()
 })
 
 test('Los cart items se crean con cantidad 1', () => {
-    const product = 'a product';
+    const product = iPad();
     const cartItem = new CartItem(product);
     expect(cartItem.quantity()).toEqual(1)
 })
 
 test('Se puede incrementar la cantidad de un cart item', () => {
-    const product = 'a product';
+    const product = iPad();
     const cartItem = new CartItem(product);
     cartItem.incrementQuantity()
     expect(cartItem.quantity()).toEqual(2)
 })
 
 test('Un cart item sabe calcular el total', () => {
-    const product = { price: 10 };
+    const product = iPad();
     const cartItem = new CartItem(product);
     cartItem.incrementQuantity()
     expect(cartItem.total()).toEqual(product.price * cartItem.quantity())
