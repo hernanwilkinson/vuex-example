af201063f431548afc85be29d3992925362b4c5f
hernan.wilkinson <hernan.wilkinson@10pines.com>
2021-10-29 17:23:16 -0300

    Refactor: Move Supermarket a Supermarket.js

--- specs/unit/Supermarket.spec.js
+++ specs/unit/Supermarket.spec.js
@@ -1,35 +1,5 @@
 import {createiPad, createTShirt, initial_ipad_inventory, removeInventaryOf} from "./TestObejectsFactory";
-
-const INVALID_PRODUCT = "Invalid product";
-
-class Supermarket {
-
-    constructor(products) {
-        this._products = products
-
-    }
-
-    availableProducts() {
-        return this._products.filter(product => product.hasStock())
-    }
-
-    // El inventario no debería ser parte del producto. Esto lo hace mutable
-    // y además se lo puede modificar sin el conocimiento del supermercado
-    // lo cual no es pensar de manera sistémica
-    decrementInventoryOf(product) {
-        this.assertIsValidProduct(product)
-
-        product.decrementInventory()
-    }
-
-    assertIsValidProduct(product) {
-        if (this._products.indexOf(product) < 0) throw new Error(INVALID_PRODUCT)
-    }
-
-    products() {
-        return this._products
-    }
-}
+import {INVALID_PRODUCT, Supermarket} from "../../src/model/Supermarket";
 
 test('availableProducts devuelve los productos con stock', () => {
     const tShirt = createTShirt()
---
+++ src/model/Supermarket.js
@@ -0,0 +1,30 @@
+export const INVALID_PRODUCT = "Invalid product";
+
+export class Supermarket {
+
+    constructor(products) {
+        this._products = products
+
+    }
+
+    availableProducts() {
+        return this._products.filter(product => product.hasStock())
+    }
+
+    // El inventario no debería ser parte del producto. Esto lo hace mutable
+    // y además se lo puede modificar sin el conocimiento del supermercado
+    // lo cual no es pensar de manera sistémica
+    decrementInventoryOf(product) {
+        this.assertIsValidProduct(product)
+
+        product.decrementInventory()
+    }
+
+    assertIsValidProduct(product) {
+        if (this._products.indexOf(product) < 0) throw new Error(INVALID_PRODUCT)
+    }
+
+    products() {
+        return this._products
+    }
+}
